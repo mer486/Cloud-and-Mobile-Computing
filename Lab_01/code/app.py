54648c453c56e34f8aa0f6c58a3fa8e3015c99ff
mer486 <merola.g@gmail.com>
2026-04-25 01:07:02 +0300

    Add CMC labs 1 to 4

---
+++ Lab_01/code/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import time
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    delay = random.expovariate(1 / 0.1)
+    time.sleep(delay)
+    return f"Response after {delay:.2f} seconds\n"
+
+app.run(host='0.0.0.0', port=5002)
